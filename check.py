d29b2c6720c8a763cac3fdff702d62b356b9e7cf
george-buckerfield <georgebuckerfield@gmail.com>
2016-11-13 17:57:24 +0000

    Lambda function to check site status

---
+++ check.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import requests
+
+def http_check(url):
+
+  check = requests.get(url)
+  
+  resp_time = check.elapsed.total_seconds()
+  resp_code = check.status_code
+
+  return resp_code, resp_time
+
+def db_get_url(site):
+  print("Get the site URL from DynamoDB")
+  return site_url
+
+def db_update(site, check_time, check_resp_code, check_resp_time):
+  print("Update DynamoDB here")
+
+def lambda_handler(event, context):
+  check_site = event['site']
+  check_url = db_get_url(check_site)
+  check_time = now()
+  
+  resp_code, resp_time = http_check(check_url)
+  db_update(check_site, check_time, resp_code, resp_time)
