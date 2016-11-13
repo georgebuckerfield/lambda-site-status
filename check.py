f576ca011c3723fc29c72024abe6383a645d7ff1
george-buckerfield <georgebuckerfield@gmail.com>
2016-11-13 18:28:00 +0000

    Get the time of the check properly

--- check.py
+++ check.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import requests
+import datetime
 
 def http_check(url):
 
@@ -16,11 +17,12 @@ def db_get_url(site):
 
 def db_update(site, check_time, check_resp_code, check_resp_time):
   print("Update DynamoDB here")
+  print(check_time, check_resp_code, check_resp_time)
 
 def lambda_handler(event, context):
   check_site = event['site']
   check_url = db_get_url(check_site)
-  check_time = now()
+  check_time = datetime.datetime.now()
   
   resp_code, resp_time = http_check(check_url)
   db_update(check_site, check_time, resp_code, resp_time)
